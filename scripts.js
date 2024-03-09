63a03a72f787165373e60616d7ed4cb7d1c9effa
Lucaslins <lucasllinsphb@gmail.com>
2024-03-09 19:40:39 -0300

    :tada: initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver o light mode, add a imagem light
+      img.setAttribute("src", "./assets/avatar-light.png")
+      alt.setAttribute( "alt","foto de Mayk Brito sorrindo, usando oculos preto, e fundo colorido")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    alt.setAttribute("alt","foto de Mayk Brito sorrindo, usando oculos, e fundo colorido")
+  }
+
+  
+  
+  // se tiver sem light mode, manter a imagem normal
+}
